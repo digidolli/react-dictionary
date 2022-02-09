699508416dadd36fb1c5fb05ba01ab57af51750b
Digidolli <iamsadekaye@gmail.com>
2022-02-09 10:51:53 +0000

    Added search engine.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      hello
+      <div className='container'>
+      <header>
+
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        Coded by digidolli
+      </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/Dictionary.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import "./Dictionary.css";
+
+export default function (){
+
+    let [keyword, setKeyword] = useState ("")
+
+    function handleKeywordChange (event) {
+        setKeyword(event.target.value);
+    }
+
+    function search(event) {
+        event.preventDefault();
+        alert (`Searching for ${keyword}`)
+
+    }
+
+    return (
+    <div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" onChange={handleKeywordChange} autoFocus={true} /> 
+        </form>
+    </div>
+)
+}
