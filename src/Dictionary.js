7024ddcfda1b9463e003c6395ff9a57431486e28
Digidolli <iamsadekaye@gmail.com>
2022-02-25 16:14:52 +0000

    Added css.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,33 +5,50 @@ import axios from "axios";
 
 export default function Dictionary (){
 
-    let [keyword, setKeyword] = useState ("")
-    let [results, setResults] = useState (null)
+    let [keyword, setKeyword] = useState ("sunset");
+    let [results, setResults] = useState (null);
+    let [loaded, setLoaded] = useState (false);
 
 
     function handleResponse (response) {
         setResults (response.data[0]);
+    }
+    
+    function search (){
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse)
+     }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();  
     }
 
     function handleKeywordChange (event) {
         setKeyword(event.target.value);
     }
 
-    function search(event) {
-        event.preventDefault();
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse)
+    function load() {
+        setLoaded(true);
+        search();
     }
-
-
-
-
+   
+    if (loaded){
     return (
     <div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="search" onChange={handleKeywordChange} autoFocus={true} /> 
+        <section>
+            <h1>What are you looking for?...</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} autoFocus={true} defaultValue="sunset"/> 
         </form>
+        <div className="hint">
+            suggested words: sunset, wine, yoga...
+        </div>
+        </section>
         <Results results={results} />
-    </div>
-)
+    </div> 
+) } else {
+    load();
+    return null;
+}
 }
