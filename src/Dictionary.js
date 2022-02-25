6233b88358a6848b848626243a1789ee7a00c95a
Digidolli <iamsadekaye@gmail.com>
2022-02-25 18:27:10 +0000

    Added Pexels API and images in correlation to  searched word.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,32 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import Results from "./Results";
 import axios from "axios";
+import Photos from "./Photos";
 
 export default function Dictionary (){
 
     let [keyword, setKeyword] = useState ("sunset");
     let [results, setResults] = useState (null);
     let [loaded, setLoaded] = useState (false);
+    let [photos, setPhotos] = useState (null);
 
 
     function handleResponse (response) {
         setResults (response.data[0]);
     }
+
+    function handlePexelsResponse (response){
+       setPhotos(response.data.photos)
+    }
     
     function search (){
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse)
+
+        let pexelsApiKey = "563492ad6f91700001000001d844e8da93234d51a0409a9d26ddccc2";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
      }
 
     function handleSubmit(event) {
@@ -46,6 +57,7 @@ export default function Dictionary (){
         </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
     </div> 
 ) } else {
     load();
